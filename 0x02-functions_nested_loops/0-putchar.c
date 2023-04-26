773967b8362b2de3ecb983250171efa4ced57a10
SV-Martins <sabrinamartins2372@gmail.com>
2023-04-26 15:05:44 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main prints _putchar
+ *
+ * discription: prints _putchar
+ *
+ * Returen 0 [sucess]
+ */
+int main(void)
+{
+	char text [9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+		
+	}
+
+	_putchar ('\n');
+
+	return(0);
+}
