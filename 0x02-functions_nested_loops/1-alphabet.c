36c4575710fabf87f32dd6faa9bd3f85f9a9bc71
SV-Martins <sabrinamartins2372@gmail.com>
2023-04-26 17:02:03 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0.
  */
+int _putchar();
 void print_alphabet(void)
 {
 	char letter = 'a';
@@ -15,5 +16,4 @@ void print_alphabet(void)
 		letter++;
 	}
 	_putchar('\n');
-	return (0);
 }
